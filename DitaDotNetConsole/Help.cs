b68443189cab7278efb26b1eedc81a67a24e98e7
Sam Grogan <sam@fusionmedia.com>
2018-11-20 09:16:05 -0800

    Project setup. Create library and console application.

---
+++ DitaDotNetConsole/Help.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dita.Net.Console
+{
+    class Help
+    {
+        // Writes out the command line help
+        public void WriteHelpToConsole() {
+            try {
+                string helpText = File.ReadAllText("Help.txt");
+                System.Console.WriteLine(helpText);
+            }
+            catch (Exception ex) {
+                System.Console.WriteLine("Error trying to read help file help.txt");
+                System.Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ DitaDotNetConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Dita.Net.Console
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // Help object, for displaying help to the users
+            Help help = new Help();
+
+            // Parse the input and see if it is valid
+            Parameters parameters = new Parameters();
+            Dictionary<string, string> config = parameters.ParseArgs(args, out bool isValid);
+
+
+            // Did we get any arguments?
+            if (isValid) {
+                // What command were we asked to perform
+                switch (config[Parameters.ARG_COMMAND]) {
+                    case Parameters.CMD_VERIFY:
+                        System.Console.WriteLine($"Verifying {config[Parameters.ARG_INPUT]}...");
+                        return 0;
+                    case Parameters.CMD_CONVERT:
+                        System.Console.WriteLine($"Converting {config[Parameters.ARG_INPUT]}...");
+                        return 0;
+                    default:
+                        break;
+                }
+            }
+
+            // If we get here then something went wrong, so exit with an error
+            help.WriteHelpToConsole();
+            return -1;
+        }
+    }
+}
